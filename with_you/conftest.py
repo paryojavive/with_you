e44874f882525c151dfd882fca241fb2fed60d8c
Yoonjae Park <paryoja@hanmail.net>
2025-09-25 23:53:11 -0700

    first awesome commit

---
+++ with_you/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from with_you.users.models import User
+from with_you.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
